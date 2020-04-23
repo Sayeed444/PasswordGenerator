9740e8c9b159f36e48041139ef9bc9f6b52458c4
sayeed444 <abusayeedamin40gmail.com>
2020-04-23 11:36:11 +0600

    Add Password Generator Project

---
+++ Generator/views.py
@@ -0,0 +1,25 @@
+import random
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+
+    return render(request,'generator/home.html')
+
+
+def password(request):
+    characters = list('abcdefghijkmnoqprstuvwxyz')
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKMNOPQRSTVUWXYZ'))
+    if request.GET.get('number'):
+        characters.extend(list('0123456789'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()'))
+
+    length = int(request.GET.get('length',14))
+    thepassword = ''
+    for x in range(length):
+       thepassword += random.choice(characters)
+    return render(request,'generator/password.html',{ 'thepassword':thepassword })
